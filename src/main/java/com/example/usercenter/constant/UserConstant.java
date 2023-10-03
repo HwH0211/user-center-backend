5f2b15e908f8139b6c8e4ff8b650fd4b682e2183
HwH0211 <1605779817@qq.com>
2023-10-03 23:19:19 +0800

    简易的完成了登录注册后端

---
+++ src/main/java/com/example/usercenter/constant/UserConstant.java
@@ -0,0 +1,24 @@
+package com.example.usercenter.constant;
+
+/**
+ * 用户常量
+ *
+ * @author weihang
+ */
+
+public interface UserConstant {
+    /**
+     * 用户登录态键
+     */
+    String USER_LOGIN_STATE = "userLoginState";
+
+    // ------- 权限 --------
+    /**
+     * 默认权限
+     */
+    int DEFAULT_ROLE = 0;
+    /**
+     * 管理员权限
+     */
+    int ADMIN_ROLE = 1;
+}
--- src/main/java/com/example/usercenter/service/UserService.java
+++ src/main/java/com/example/usercenter/service/UserService.java
@@ -2,19 +2,42 @@ package com.example.usercenter.service;
 
 import com.example.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.sun.net.httpserver.HttpServer;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 /**
- *用户服务
+ * 用户服务
+ *
  * @author Rain
-*/
+ */
 public interface UserService extends IService<User> {
+
     /**
      * 用户注释
      *
-     * @param userAccount 用户账户
-     * @param userPassword 用户密码
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
      * @param checkPassword 校验密码
      * @return 新用户id
      */
-    long userRegister(String userAccount,String userPassword,String checkPassword);
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     *用户登录
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @return 脱敏后的用户信息
+     */
+    User userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 用户脱敏
+     *
+     * @param originUser
+     * @return
+     */
+    User getSafetyUser(User originUser);
 }
